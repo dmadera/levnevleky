dc7ad3fd04eebc72d9ed0bf6cfb5962204b531ef
Daniel Maděra <madera.dan@gmail.com>
2017-12-06 18:47:49 +0100

    Welcome

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -4,6 +4,11 @@ import lift from 'assets/img/lift-icon.png';
 import open from 'assets/img/opened.svg';
 import close from 'assets/img/closed.svg';
 import family from 'assets/img/family.png';
+import groomer from 'assets/img/groomer.png';
+import track1 from 'assets/img/track1.svg';
+import track2 from 'assets/img/track2.svg';
+import track3 from 'assets/img/track3.svg';
+import tracks from 'assets/img/tracks.svg';
 import Navbar from 'components/navbar.js';
 
 class Welcome extends React.PureComponent {
@@ -43,25 +48,53 @@ class Welcome extends React.PureComponent {
                   </div>
                 </div>
               </div>
-              <div class="col-md-3">
+              <div class="col-md-3 sjezdovky">
                 <div class="card">
-                  <div class="card-header">Vleky</div>
-                  <ul class="list-group list-group-flush">
-                    <li class="list-group-item">
-                      <img src={lift} />
-                      KOTVA 500m
-                      <img src={open} class="float-right" alt="V provozu" />
-                    </li>
-                    <li class="list-group-item">
-                      <img src={lift} />
-                      POMA 300m
-                      <img src={close} class="float-right" alt="V provozu" />
-                    </li>
-                  </ul>
-                  <div class="card-footer">
-                    <a href="#" class="btn btn-dark">Více o areálu</a>
-                  </div>
-
+                  <table class="table">
+                    <tr class="card-header">
+                      <th colspan="4">Sjezdovky</th>
+                    </tr>
+                    <tr>
+                      <td>
+                        <img src={track1} alt="V provozu" />
+                        </td><td>střední</td><td>
+                        <img src={open} alt="V provozu" />
+                      </td>
+                      <td>
+                        <img src={groomer} alt="V provozu" />
+                      </td>
+                      </tr>
+                      <tr>
+                      <td>
+                        <img src={track2} alt="V provozu" />
+                        </td><td>lehká</td><td>
+                        <img src={open} alt="V provozu" />
+                      </td>
+                      <td>
+                        <img src={groomer} alt="V provozu" />
+                      </td>
+                      </tr>
+                      <tr>
+                      <td>
+                        <img src={track3} alt="V provozu" />
+                        </td><td>lehká</td><td>
+                        <img src={open} alt="V provozu" />
+                      </td>
+                      <td>
+                        <img src={groomer} alt="V provozu" />
+                      </td>
+                      </tr>
+                      <tr>
+                      <td>
+                        <img src={tracks} alt="V provozu" />
+                        </td><td>snowpark</td><td>
+                        <img src={open} alt="V provozu" />
+                      </td>
+                      <td>
+                        <img src={groomer} alt="V provozu" />
+                      </td>
+                    </tr>
+                  </table>
                 </div>
               </div>
               <div class="col-md-3">
