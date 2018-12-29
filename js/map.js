a1f82c57e9a0ee84004c072ac7db2d3e26472ce5
Daniel Maděra <madera.dan@gmail.com>
2018-12-29 01:04:20 +0100

    Map, area, mobile optimaze

---
+++ js/map.js
@@ -0,0 +1,7 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map_container'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+}
+        
