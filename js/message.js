64e50355eb9ad49cd0d7a48c68aca4abca90be56
Daniel Maděra <madera.dan@gmail.com>
2019-01-02 03:31:26 +0100

    DV

---
+++ js/message.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+  //showMessage('signup-success', null);
+});
+
+function showMessage(type, details) {
+  var modal = $('#modal-info');
+
+  switch(type) {
+    case 'message-success':
+      setModalData(modal, "Zpráva", `
+        Děkujeme za zprávu.<br>
+        Budeme Vás kontaktovat na email ihned, jak to bude možné.<br>
+      `);
+      break;
+
+    case 'message-failure':
+      setModalData(modal, "Chyba odeslání zprávy", `
+        Zprávu se nepodařilo odeslat.
+        Zkotrolujte, zda máte správně vyplněný formulář z zkuste to znovu.<br>
+        V případě dalšího selhání nás kontaktujte, prosím, přímo na emailu - info@levnevleky.cz.<br>
+        Děkujeme.<br>
+        <br>
+        Detail chyby: ` + JSON.stringify(details) + `
+      `);
+      break;
+      
+    default:
+      setModalData(modal, "Zpráva", `Ahoj tanečníku!`);
+  }
+  modal.modal('show');
+}
+
+function setModalData(modal, title, body) {
+  modal.find(".modal-title").text(title);
+  modal.find(".modal-text").html(body);
+}
