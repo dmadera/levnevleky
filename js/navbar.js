56e3d7d20ecad6fb7a0d3ae290b824c235402e52
Daniel Maděra <madera.dan@gmail.com>
2019-02-04 19:57:37 +0100

    Multiple changes

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,9 @@
 $(window).scroll(function(e) {
   if($(window).scrollTop() < 80) {
     $(".navbar").addClass("top");
+    $(".subtitle").addClass("d-lg-block");
   } else {
     $(".navbar").removeClass("top");
+    $(".subtitle").removeClass("d-lg-block");
   }
 });
